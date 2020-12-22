9b406028655284fc095cc0c6600e39901c006893
MiraculousMoon <lgt986452565@gmail.com>
2020-12-22 13:40:31 +0800

    modify icon distance

--- .dwmblocks/blocks.h
+++ .dwmblocks/blocks.h
@@ -1,17 +1,17 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/	 	/*Update Interval*/	/*Update Signal*/
-    {"   ", "~/.dwmblocks/scripts/kernel",		    360,		        2},
+    {"  ", "~/.dwmblocks/scripts/kernel",		    360,		        2},
 
-	{"   ", "~/.dwmblocks/scripts/upt",		        60,		            2},
+	{"  ", "~/.dwmblocks/scripts/upt",		        60,		            2},
 
-	{"   ", "~/.dwmblocks/scripts/pacupdate",		360,		        9},
+	{"  ", "~/.dwmblocks/scripts/pacupdate",		360,		        9},
 	
-	{"   ", "~/.dwmblocks/scripts/memory",	        6,		            1},
+	{"  ", "~/.dwmblocks/scripts/memory",	        6,		            1},
 
-	{"   ", "~/.dwmblocks/scripts/volume",			2,		            10},
+	{"  ", "~/.dwmblocks/scripts/volume",			2,		            10},
 
-	{"   ", "~/.dwmblocks/scripts/clock",			5,		            0},
+	{"  ", "~/.dwmblocks/scripts/clock",			5,		            0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
